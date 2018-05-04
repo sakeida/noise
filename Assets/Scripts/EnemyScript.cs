38d1112661a8cdc033456d928cc0b3f551d84c95
sakeida <>
2018-05-04 22:22:27 +0900

    α版提出 実装内容 ・プレイヤーの特殊能力『スローモーション（F）』『巻き戻し（マウス右クリック）』を追加するスクリプト ・鍵を４つとることでドアがあくようなスクリプト ・タイトル画面、ゲームオーバー画面、そして死んだ箇所からのリスボーン。 以上です。

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+//
+using UnityEngine.UI;
+//
+
+public class EnemyScript : MonoBehaviour {
+	//public Transform target;
+	//NavMeshAgent agent;
+	int enemyHP=3;
+	//
+	//public Slider slider;
+
+	//
+
+	// Use this for initialization
+	void Start () {
+		/*GameObject player = GameObject.Find ("FPSController");
+		target = player.transform;
+		agent = GetComponent<NavMeshAgent> ();*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	agent.SetDestination (target.position);
+		
+	}
+	void Damage(){
+		enemyHP--;
+		//
+	//	slider.value-=1;
+		//
+
+		if (enemyHP == 0) {
+			Destroy (this.gameObject);
+
+		}
+	}
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag=="player"){
+			col.SendMessage ("playerDamage");
+			Destroy (this.gameObject);
+
+		}
+	}
+}
---
+++ Assets/Scripts/TimeBody.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TimeBody : MonoBehaviour {
+	public bool isRewinding = false;
+	List<PointInTime>pointsInTime;
+	Rigidbody rb;
+
+
+	void Start () {
+		pointsInTime = new List<PointInTime>();
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(1)){
+			startRewind ();
+	}
+		if (Input.GetMouseButtonUp (1)) {
+			stopRewind ();
+		}
+	}
+	void FixedUpdate(){
+		if (isRewinding)
+			Rewind ();
+		else
+			Record();
+	}
+	void Rewind(){
+		if (pointsInTime.Count > 0) {
+			PointInTime pointInTime = pointsInTime [0];
+			transform.position = pointInTime.position;
+			transform.rotation = pointInTime.rotation;
+			pointsInTime.RemoveAt(0);
+		} else {
+			stopRewind ();
+		}
+	}
+
+
+
+	void Record(){
+		pointsInTime.Insert (0, new PointInTime(transform.position,transform.rotation));
+	}
+
+
+	public void startRewind(){
+		isRewinding = true;
+		rb.isKinematic = true;
+	}
+	public void stopRewind(){
+		isRewinding = false;
+		rb.isKinematic = false;
+	}
+
+
+}
